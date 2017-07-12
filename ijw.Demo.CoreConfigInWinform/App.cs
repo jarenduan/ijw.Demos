bcf7d25c0468680dd2bc19ff546e0a66e755179d
jaren@5510 <jarenduan@gmail.com>
2017-07-12 20:13:21 +0800

    add coreconfigInWinform

---
+++ ijw.Demo.CoreConfigInWinform/App.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ijw.Demo.CoreConfigInWinform {
+    public class App {
+        private readonly IMainForm _mainForm;
+
+        public App(IMainForm mainForm) {
+            this._mainForm = mainForm;
+        }
+        public void Run() {
+            Application.Run(this._mainForm as Form);
+        }
+    }
+}
---
+++ ijw.Demo.CoreConfigInWinform/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ijw.Demo.CoreConfigInWinform {
+    public partial class MainForm : Form, IMainForm {
+        private MainFormSettings _settings;
+
+        public MainFormSettings Settings { get => _settings; set => _settings = value; }
+
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        public MainForm(MainFormSettings settings) : this() {
+            this.Settings = settings;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            this.Text = this.Settings.Name;
+        }
+    }
+}
